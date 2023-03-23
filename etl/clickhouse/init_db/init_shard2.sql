cd2e433d8b028bb9377ccaadbc09ee77874f7797
arturkukobin <kukobinartur@yandex.ru>
2023-03-23 21:16:32 +0300

    тестовый вариант

---
+++ etl/clickhouse/init_db/init_shard2.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE IF NOT EXISTS shard;
+CREATE DATABASE IF NOT EXISTS replica;
+
+CREATE TABLE IF NOT EXISTS shard.kafka_film_timestamp (
+      user_id String,
+      film_id String,
+      viewed_frame Int16,
+      event_time DateTime('Europe/Moscow'))
+      ENGINE = Kafka
+      SETTINGS kafka_broker_list = 'broker:29092',
+               kafka_topic_list = 'films-timestamps',
+               kafka_group_name = 'timestamp-viewers-group',
+               kafka_format = 'JSONEachRow',
+               kafka_commit_every_batch = 1,
+               kafka_num_consumers = 3,
+               kafka_max_block_size = 10000,
+               kafka_poll_max_batch_size = 10000,
+               kafka_thread_per_consumer = 1,
+               kafka_handle_error_mode = 'stream';
+
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS consumer TO shard.film_timestamp
+      AS SELECT * FROM  shard.kafka_film_timestamp;
+
+CREATE TABLE IF NOT EXISTS shard.film_timestamp (
+      user_id String,
+      film_id String,
+      viewed_frame Int16,
+      event_time DateTime('Europe/Moscow'))
+      Engine=ReplicatedMergeTree('/clickhouse/tables/shard2/film_timestamp', 'replica_1') PARTITION BY toYYYYMMDD(event_time) ORDER BY user_id;
+
+CREATE TABLE IF NOT EXISTS replica.film_timestamp (
+      user_id String,
+      film_id String,
+      viewed_frame Int16,
+      event_time DateTime('Europe/Moscow'))
+      Engine=ReplicatedMergeTree('/clickhouse/tables/shard1/film_timestamp', 'replica_2') PARTITION BY toYYYYMMDD(event_time) ORDER BY user_id;
+
+CREATE TABLE IF NOT EXISTS default.film_timestamp (
+      user_id String,
+      film_id String,
+      viewed_frame Int16,
+      event_time DateTime('Europe/Moscow'))
+      ENGINE = Distributed('company_cluster', '', film_timestamp, rand());
---
+++ etl/main.py
@@ -0,0 +1,49 @@
+import random
+from faker import Faker
+from kafka import KafkaProducer
+from time import sleep
+import pickle
+import datetime
+import json
+
+# data = {'user_id': 'qwe', 'film_id': 'qwe', 'viewed_frame': random.randint(0, 180),
+#         'event_time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda v: json.dumps(v, ensure_ascii=False).encode('utf-8'))
+
+faker = Faker()
+# dates = []
+# keys = []
+for _ in range(100):
+    year = 2022
+    month = 5
+    day = 24
+    hour = random.randint(0, 23)
+    minute = random.randint(0, 59)
+    second = random.randint(0, 59)
+    random_date = datetime.datetime(year, month, day, hour, minute, second)
+    formatted_date = random_date.strftime('%Y-%m-%d %H:%M:%S')
+    data = {'user_id': str(faker.uuid4()), 'film_id': str(faker.uuid4()), 'viewed_frame': random.randint(0, 180),
+            'event_time': formatted_date}
+
+
+    b = f'{data["user_id"]}+{data["film_id"]}'
+    c = pickle.dumps(b)
+    producer.send(
+        'films-timestamps',
+        value=data,
+        key=c,
+    )
+    sleep(0.2)
+#     dates.append(data)
+#     keys.append(c)
+#
+#
+# for index, value in enumerate(dates):
+#     producer.send(
+#         'films-timestamps',
+#         value=value,
+#         key=keys[index],
+#     )
+
